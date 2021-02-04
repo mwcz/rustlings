6e5706d9922bce190723563ffd6766aa82b37fb8
mwcz <mwc@clayto.com>
2021-02-03 21:49:30 -0500

    my first 47 embafflements

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,13 +2,13 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` for hints!
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    let mut a = [0; 100];
+
+    a[10] = 42;
 
     if a.len() >= 100 {
-        println!("Wow, that's a big array!");
+        println!("Wow, that's a big array! {:?}", a);
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
     }
